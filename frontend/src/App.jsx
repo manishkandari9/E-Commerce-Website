602f7d5d9484f2c7e6f3e5bb2d2b254b142b0e2d
manish kandari <manishkandari43@gmail.com>
2025-05-30 10:17:43 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,7 @@ import BentoCard from './components/page/BentoCard';
 import Footer from './components/page/footer';
 import FooterSkeleton from './Skelton/footer-skeleton';
 import BookingForm from './components/page/Booking';
-//import BookingSkeletonLoader from './Skelton/bookingSkeletonLoader';
+// import BookingSkeletonLoader from './Skelton/bookingSkeletonLoader';
 import SmartItineraryGenerator from './pages/smart-itinerary-generator';
 function App() {
   return (
@@ -47,7 +47,7 @@ function App() {
           <Route path="/footer" element={<Footer />} />
           <Route path="/footer-skeleton" element={<FooterSkeleton />} />
           <Route path="/booking" element={<BookingForm />} />
-          //<Route path="/Booking-Skeleton-Loader" element={<BookingSkeletonLoader />} />
+          // <Route path="/Booking-Skeleton-Loader" element={<BookingSkeletonLoader />} />
           <Route path="/smart-itinerary-generator" element={<SmartItineraryGenerator />} />
         </Routes>
       </div>
